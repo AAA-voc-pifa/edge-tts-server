1fbf975cd3a00ed993707ced0d437f5f9d124856
PPz <ppzreboot@qq.com>
2026-09-07 18:28:00 +0800

    feat: auth

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 from service.edge_tts import voice_mng, tts_stream
 from edge_tts.typing import VoicesManagerVoice
-from edge_tts import Communicate
+from service.auth import make_verify
 
-app = FastAPI()
+app = FastAPI(
+	dependencies=[
+		make_verify(),
+	]
+)
 
 @app.get('/voice')
 async def all_voices(req: Request) -> list[VoicesManagerVoice]:
---
+++ service/auth.py
@@ -0,0 +1,16 @@
+import os
+from typing import Annotated
+from fastapi import Depends, Header, status, HTTPException
+
+def make_verify():
+	pw = os.getenv('password')
+	if pw is None or pw == '':
+		raise ValueError('（环境变量）未设置访问密码')
+
+	def verify(password: Annotated[str, Header()]):
+		if password != pw:
+			raise HTTPException(
+				status_code=status.HTTP_403_FORBIDDEN,
+				detail='密码错误'
+			)
+	return Depends(verify)
